04a371125826ab8f58804051e9749a50da9420bc
prarthana <it24100212@my.sliit.lk>
2026-04-03 18:42:00 +0530

    update boarding manager auth role

--- routes/boardingRoutes.js
+++ routes/boardingRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 router.use(protect);
 
-router.get('/', authorizeRoles('PetSitter', 'Admin'), getAllBoardingBookings);
-router.put('/:id/status', authorizeRoles('PetSitter', 'Admin'), updateBookingStatus);
+router.get('/', authorizeRoles('BoardingManager', 'Admin'), getAllBoardingBookings);
+router.put('/:id/status', authorizeRoles('BoardingManager', 'Admin'), updateBookingStatus);
 router.get('/available', getAvailableSlots);
 router.post('/lock', lockSlot);
 router.post('/confirm', confirmBooking);
